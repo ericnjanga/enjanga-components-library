81ee1e07520af377c44ecb9ec4314e236fcf2a35
Eric Njanga <eric.njanga@gmail.com>
2025-06-21 07:37:46 -0400

    chore: Storybook files cleanup

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,7 +5,7 @@ const config: StorybookConfig = {
     // 1) Listing MDX files in explicit order
     '../src/stories/getting-started/Welcome.mdx',
     '../src/stories/getting-started/FeatureFlags.mdx',
-    
+
     // 2) Including components
     // Exported components don't need story files, that's why their stories are located inside the "stories" folder.
     '../src/stories/components/**/*.stories.@(js|jsx|ts|tsx|mdx)',
@@ -13,9 +13,8 @@ const config: StorybookConfig = {
   addons: [
     '@storybook/addon-docs', // ← required for MDX support
     '@storybook/addon-essentials', // Includes docs, controls, etc.
-    // '@storybook/addon-interactions',
     '@storybook/addon-themes',
-    '@storybook/addon-mdx-gfm'
+    '@storybook/addon-mdx-gfm',
   ],
   framework: {
     name: '@storybook/nextjs',
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,16 +1,13 @@
 import type { Preview } from '@storybook/react';
-import '../src/app/globals.scss';
-import { CustomDocsPage } from './CustomDocsPage';
+/**
+ * Carbon styles load only in Storybook
+ * They don’t pollute your final npm package
+ */
+import '@carbon/styles/css/styles.min.css'; // Carbon global styles
+import '../src/styles/index.scss'; // Component library styles
 
 const preview: Preview = {
   parameters: {
-    // docs: {
-    //   page: CustomDocsPage,
-    //   toc: {
-    //     headingSelector: 'h2, h3',
-    //     title: 'On this page',
-    //   },
-    // },
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
