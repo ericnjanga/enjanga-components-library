859c9e9dce435dcb9a373f4d5919ad350a4a0e33
Eric Njanga <eric.njanga@gmail.com>
2025-06-21 07:33:58 -0400

    chore: updated components export list

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,5 @@
  */
 export { default as CustomIcon } from './CustomIcon';
 export { CustomTile } from './CustomTile';
-
+export { HeadlinedList } from './HeadlinedList';
+export { List } from './List';
