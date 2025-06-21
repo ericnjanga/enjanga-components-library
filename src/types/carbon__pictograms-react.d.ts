5a01b6455c4e876be0e06a618737fa269030a336
Eric Njanga <eric.njanga@gmail.com>
2025-06-21 07:27:19 -0400

    chore: fixing carbon typescript recognition

--- src/types/carbon__pictograms-react.d.ts
+++ src/types/carbon__pictograms-react.d.ts
@@ -1,14 +1,7 @@
 declare module '@carbon/pictograms-react' {
-  const content: any;
-  export default content;
-}
-
-declare module '@carbon/pictograms-react' {
   import * as React from 'react';
 
   export const Hills: React.FC<React.SVGProps<SVGSVGElement>>;
   export const AppDeveloper: React.FC<React.SVGProps<SVGSVGElement>>;
   export const Leadership: React.FC<React.SVGProps<SVGSVGElement>>;
-
-  // Add more pictograms as needed
 }
