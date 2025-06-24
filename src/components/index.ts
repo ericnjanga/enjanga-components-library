7702b2d1d2409fb5d3b9e518d037b4842ccf235c
Eric Njanga <eric.njanga@gmail.com>
2025-06-24 06:20:17 -0400

    chore: exporting banner component

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,6 +6,7 @@ import ContactButton, { contactButtonIconsList } from './ContactButton';
  * The list of components that the library exposes to the outside world
  */
 export { BrandName } from './BrandName';
+export { Banner } from './Banner';
 export { default as ContactButton } from './ContactButton';
 export { default as CustomIcon } from './CustomIcon';
 export { CustomTile } from './CustomTile';
