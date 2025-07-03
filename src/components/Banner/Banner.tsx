1981bcd95cfccd7dc8487d27c0ccc2b5a65081cd
Eric Njanga <eric.njanga@gmail.com>
2025-07-03 09:55:46 -0400

    chore: banner component update

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -12,11 +12,12 @@ import { Grid, Column } from '@carbon/react';
 interface BannerProps {
   title: string;
   subtitle?: string;
+  className?: string;
 }
 
-const Banner = ({ title, subtitle }: BannerProps) => {
+const Banner = ({ title, subtitle, className }: BannerProps) => {
   return (
-    <header className={clsx('enj-Banner')}>
+    <header className={clsx('enj-Banner', className)}>
       <Grid fullWidth>
         <Column lg={8} md={6} sm={4}>
           <h1>{title}</h1>
