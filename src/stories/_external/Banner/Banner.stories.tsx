71b7581d442dce9762593ae1c897a24e1935ae84
Eric Njanga <eric.njanga@gmail.com>
2025-07-28 07:55:19 -0400

    chore: add skeleton animation to Banner component

--- src/stories/_external/Banner/Banner.stories.tsx
+++ src/stories/_external/Banner/Banner.stories.tsx
@@ -8,13 +8,17 @@ const meta: Meta<typeof Banner> = {
   argTypes: {
     title: { control: 'text' },
     subtitle: { control: 'text' },
+    isJumbtron: {
+      control: 'select',
+      options: [true, false],
+    },
   },
   args: {
-    title: 'Bridging Design & Code',
-    subtitle: `With 15+ years of experience, I specialize in transforming complex 
-    business requirements into intuitive, high-performance web applications. 
-    I lead end-to-end UX and front-end development through agile, cross-functional 
-    collaboration—delivering results that drive business impact.`,
+    title: 'Toffee jujubes candy jujubes bears',
+    subtitle: `Gingerbread cupcake candy canes sugar plum I love soufflé.
+                  Jelly beans sweet roll shortbread wafer shortbread. Shortbread
+                  caramels I love I love bear claw jelly beans. Danish liquorice
+                  halvah brownie I love cookie dessert brownie jelly beans.`,
     isJumbtron: true,
   },
 };
@@ -24,3 +28,58 @@ export default meta;
 type Story = StoryObj<typeof Banner>;
 
 export const Default: Story = {};
+
+export const DefaultNoSubtitle: Story = {
+  args: {
+    subtitle: `none`,
+  },
+};
+
+export const SmallBanner: Story = {
+  args: {
+    isJumbtron: false,
+    title: 'Toffee jujubes candy jujubes bears',
+    subtitle: `Gingerbread cupcake candy canes sugar plum I love soufflé.
+                  Jelly beans sweet roll shortbread wafer shortbread.`,
+  },
+};
+
+export const SmallBannerNoSubtitle: Story = {
+  args: {
+    isJumbtron: false,
+    title: 'Toffee jujubes candy jujubes bears',
+    subtitle: `none`,
+  },
+};
+
+export const SkeletonLoaderDefault: Story = {
+  args: {
+    title: undefined,
+    // The animated skeleton will show up if "title" props are undefined
+  },
+};
+
+export const SkeletonLoaderDefaultNoSubtitle: Story = {
+  args: {
+    title: undefined,
+    subtitle: `none`,
+    // The animated skeleton will show up if "title" props are undefined
+  },
+};
+
+export const SkeletonLoaderSmall: Story = {
+  args: {
+    title: undefined,
+    isJumbtron: false,
+    // The animated skeleton will show up if "title" props are undefined
+  },
+};
+
+export const SkeletonLoaderSmallNoSubtitle: Story = {
+  args: {
+    title: undefined,
+    isJumbtron: false,
+    subtitle: `none`,
+    // The animated skeleton will show up if "title" props are undefined
+  },
+};
