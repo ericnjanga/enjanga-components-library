c5346c97bdd8e27ec4676660bf9fb7469e3f5084
Eric Njanga <eric.njanga@gmail.com>
2025-08-09 06:24:34 -0400

    chore: add list-item to skeleton animation

--- src/components/SkeletonAnimation/SkeletonAnimation.tsx
+++ src/components/SkeletonAnimation/SkeletonAnimation.tsx
@@ -1,22 +1,37 @@
 import clsx from 'clsx';
+import { SkeletonAnimationProps } from './libs/types';
 
-interface SkeletonAnimationProps {
-  className?: string;
-  part: 'heading' | 'body';
-}
+const SkeletonListItem = () => (
+  <div className="skeleton-list-item skeleton-bot-spacing-2">
+    <p className="skeleton skeleton-dot"></p>
+    <p className="skeleton skeleton-text"></p>
+  </div>
+);
 
 const SkeletonAnimation = ({ className, part }: SkeletonAnimationProps) => {
   return (
     <>
       {part === 'heading' && (
-        <h1 className={clsx(className, 'skeleton skeleton-title')}></h1>
+        <h1
+          className={clsx(
+            className,
+            'skeleton skeleton-title skeleton-bot-spacing-1'
+          )}
+        ></h1>
       )}
       {part === 'body' && (
         <div className={clsx(className, 'skeleton-text-wrapper')}>
-          <p className="skeleton skeleton-text"></p>
-          <p className="skeleton skeleton-text"></p>
-          <p className="skeleton skeleton-text"></p>
-          <p className="skeleton skeleton-text"></p>
+          <p className="skeleton skeleton-text skeleton-bot-spacing-2"></p>
+          <p className="skeleton skeleton-text skeleton-bot-spacing-2"></p>
+          <p className="skeleton skeleton-text skeleton-bot-spacing-2"></p>
+          <p className="skeleton skeleton-text skeleton-bot-spacing-2"></p>
+        </div>
+      )}
+      {part === 'list' && (
+        <div className={clsx(className, 'skeleton-text-wrapper')}>
+          <SkeletonListItem />
+          <SkeletonListItem />
+          <SkeletonListItem />
         </div>
       )}
     </>
