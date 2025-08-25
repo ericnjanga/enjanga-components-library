f0ebbf8e49c15f319147c380a6f2e23ed1fe29e6
Eric Njanga <eric.njanga@gmail.com>
2025-08-25 04:24:34 -0400

    chore: unecessary code cleanup

--- src/components/Banner/libs/types.ts
+++ src/components/Banner/libs/types.ts
@@ -1,7 +1,5 @@
 // Banner props type
 // ----------------
-import type { Node } from '@contentful/rich-text-types';
-import { HDG_levelPropsType } from '@/components/Heading/libs/types';
 import { FTX_propsType } from '@/components/FeatureText/libs/types';
 
 export interface BNN_propsType {
