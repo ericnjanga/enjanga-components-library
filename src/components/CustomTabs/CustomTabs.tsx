f05829c355e14bb2b58ef4e0ffeb7dcf4e20ed15
Eric Njanga <eric.njanga@gmail.com>
2025-09-21 06:03:25 -0400

    chore: component troubleshooting

--- src/components/CustomTabs/CustomTabs.tsx
+++ src/components/CustomTabs/CustomTabs.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from 'react';
 import { Tabs, TabList, Tab, TabPanels, TabPanel } from '@carbon/react';
 
@@ -5,21 +7,18 @@ const CustomTabs = () => {
   const [selected, setSelected] = useState(0);
 
   return (
-    <Tabs
-        selectedIndex={selected}
-        onChange={({ selectedIndex }) => setSelected(selectedIndex)}
-      >
-        <TabList aria-label="Sample Tabs">
-          <Tab>Tab One</Tab>
-          <Tab>Tab Two</Tab>
-          <Tab>Tab Three</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>Content for Tab One</TabPanel>
-          <TabPanel>Content for Tab Two</TabPanel>
-          <TabPanel>Content for Tab Three</TabPanel>
-        </TabPanels>
-      </Tabs>
+    <Tabs selectedIndex={selected} onChange={({ selectedIndex }) => setSelected(selectedIndex)}>
+      <TabList aria-label="Sample Tabs">
+        <Tab>Tab One *</Tab>
+        <Tab>Tab Two #</Tab>
+        <Tab>Tab Three</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>Content for Tab One</TabPanel>
+        <TabPanel>Content for Tab Two</TabPanel>
+        <TabPanel>Content for Tab Three</TabPanel>
+      </TabPanels>
+    </Tabs>
   );
 };
 
