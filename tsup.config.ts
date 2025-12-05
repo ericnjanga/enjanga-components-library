0fc5506353e93254dc05a55f4d33ed01816ee619
Eric Njanga <eric.njanga@gmail.com>
2025-12-04 22:32:42 -0500

    fix: updating build process

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
-// enjanga-components-library/tsup.config.ts
-
+// tsup.config.ts
 import { defineConfig } from "tsup";
 import shared from "enjanga-core-setup/tsup.shared.js";
+import { sassPlugin } from "esbuild-sass-plugin";
 
 export default defineConfig({
   entry: ["src/index.ts"],
@@ -10,12 +10,20 @@ export default defineConfig({
   outDir: "dist",
   clean: false, // keep dist/styles.css
   external: [...shared.externals, "react-is"],
-  noExternal: ["react-is"], // 👈 make sure esbuild does NOT inline it
+  noExternal: ["react-is"],
   treeshake: true,
   splitting: false,
   esbuildOptions(options) {
     options.platform = "neutral";
-    options.mainFields = ["module", "main"]; // 👈 force ESM resolution
+    options.mainFields = ["module", "main"];
     return options;
   },
+  esbuildPlugins: [
+    sassPlugin({
+      // "style" = injects <style> tags at runtime when the JS bundle is loaded
+      // This works well for component-level styles.
+      type: "style",
+      // optional: loadPaths: ["src", "node_modules"],
+    }),
+  ],
 });
